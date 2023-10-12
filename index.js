927a6bcae092d37b45777238c0cf3ed07614a4e1
WPinoCaico <williamspinocaico@gmail.com>
2023-10-12 16:17:02 -0500

    se hizo prueba

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello llWorld!')
 })
+
+app.get('/wjpc', (req, res) => {
+  res.send('Hello Wsadd!')
+})
+
+app.get('/puerto', (req, res) => {
+  res.send('Hello puerto!')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
